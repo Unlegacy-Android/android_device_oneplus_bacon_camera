1f7d347586506955df00454acb4e8a28a941b73a
Suman Mukherjee <sumam@codeaurora.org>
2017-04-21 12:03:10 +0530

    QCamera2: Fixed compilation error for AOSP upgrade
     Fixed compilation error for O
    
    Change-Id: I119310169015f5713ff78df090935777a8350795
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- QCamera2/HAL/Android.mk
+++ QCamera2/HAL/Android.mk
@@ -17,7 +17,7 @@ LOCAL_SRC_FILES := \
         QCameraThermalAdapter.cpp \
         wrapper/QualcommCamera.cpp
 
-LOCAL_CFLAGS = -Wall -Wextra -Werror
+LOCAL_CFLAGS = -Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-variable
 
 LOCAL_CFLAGS += -DHAS_MULTIMEDIA_HINTS
 
@@ -61,7 +61,7 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libnativewindow
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
