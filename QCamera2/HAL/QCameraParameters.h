a3f9250fd9dfe24c7d88e1afbf4809d046ed1c08
Sultanxda <sultanxda@gmail.com>
2017-07-08 12:34:32 -0700

    QCamera2: Remove ISO-based antishake
    Due to the unstable nature of how the ISO was being changed (i.e. there are
    a bunch of hacks needed to change the ISO with our camera stack), this
    feature is unstable and results in the camera preview becoming severely
    underexposed after capturing photos sometimes.
    
    The amount of motion blur reduction this provides is negligible (since
    our chromatix library for the camera preview caps exposure at 1/30th of
    a second), so just remove the feature entirely.

--- QCamera2/HAL/QCameraParameters.h
+++ QCamera2/HAL/QCameraParameters.h
@@ -604,8 +604,6 @@ public:
     int32_t  updateCurrentFocusPosition(int32_t pos);
     bool isDisplayFrameNeeded() { return m_bDisplayFrame; };
     int32_t setDisplayFrame(bool enabled) {m_bDisplayFrame=enabled; return 0;};
-    void setPrvwIsoMode(int32_t isoValue);
-    int32_t getPrvwIsoMode();
 
 private:
     int32_t setPreviewSize(const QCameraParameters& );
@@ -842,7 +840,6 @@ private:
     int32_t mHfrMode;
     bool m_bDisplayFrame;
     int32_t mExposureTime;
-    int32_t mPrvwIsoMode;
     int32_t mManualIso;
     bool m_bSceneModeAuto;
 };
