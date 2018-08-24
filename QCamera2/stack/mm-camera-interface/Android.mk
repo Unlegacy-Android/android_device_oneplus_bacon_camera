022e7c8620303968a2ee6a78fcd860ca7f993be2
Gianmarco Rengucci <rengucci.gianmarco@gmail.com>
2018-08-24 15:20:29 +0200

    camera: enable CLANG globally
    Change-Id: Ie064b2daece97f3010f620ee1a2b41acafd2247f
    Signed-off-by: Gianmarco Rengucci <rengucci.gianmarco@gmail.com>

--- QCamera2/stack/mm-camera-interface/Android.mk
+++ QCamera2/stack/mm-camera-interface/Android.mk
@@ -54,6 +54,7 @@ LOCAL_MODULE           := libmmcamera_interface
 LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog
 LOCAL_MODULE_TAGS := optional
+LOCAL_CLANG := true
 
 include $(BUILD_SHARED_LIBRARY)
 
